c10234058552048ab8d9c68656569f0246c5bdfd
Robert Mews <robert.mews@gmail.com>
2017-12-23 20:34:19 -0600

    added map markers

--- js/app.js
+++ js/app.js
@@ -41,27 +41,67 @@ var wineries = [
 ];
 
 var Winery = function(data) {
-    this.name = ko.observable(data.name);
+    this.name = data.name;
 };
 
+
+////////// Google Maps API /////////
+
+// global map variable
 var map;
 
+// blank array for map markers
+var markers = [];
+
+// initialize map
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
+    // Constructor creates a new map
     map = new google.maps.Map(document.getElementById('map'), {
+        // center the map
         center: {
             lat: 38.297539,
             lng: -122.286865
         },
+        // set zoom level for map
         zoom: 10
     });
+
+    var bounds = new google.maps.LatLngBounds();
+
+    // loop through locations and set markers
+    for (var i = 0; i < wineries.length; i++) {
+        // Get the location and name from wineries array.
+        var location = wineries[i].location;
+        var name = wineries[i].name;
+
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: location,
+            title: name,
+            animation: google.maps.Animation.DROP
+          });
+
+        // Push the marker to the array of markers.
+        markers.push(marker);
+
+        // extend boundries of map for each marker
+        bounds.extend(markers[i].position);
+    }
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
 }
+
+
+////////// View Model //////////
 
 var ViewModel = function() {
     var self = this;
 
+    // set empty observable array
     self.wineryList = ko.observableArray([]);
 
+    // call on the locations and push to observable array
     wineries.forEach(function(winery) {
         self.wineryList.push(new Winery(winery));
     });
